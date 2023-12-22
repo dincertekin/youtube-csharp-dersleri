a9603d95720e0f3ff251c11b58f7f585568d426e
Dinçer Tekin <dincertekin@protonmail.com>
2023-12-22 19:55:23 +0200

    Create Program.cs

---
+++ ders-4/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace ilkprojemiz
+{
+  internal class Program
+  {
+    static void Main(String[] args)
+    {
+      // 0'dan 99'a kadar saydıralım.
+      for (int i = 0; i 100; i++)
+      {
+        Console.WriteLine(i);
+      }
+
+      // Tüm sayıları sırasıyla ekrana yazdıralım.
+      int[] sayilar = { 1, 4, 9, 12, 5, 7 };
+      for (int i = 0; i < sayilar.Length; i++)
+      {
+        Console.WriteLine(sayilar[i]);
+      }
+      
+      Console.ReadKey();
+    }
+  }
+}
