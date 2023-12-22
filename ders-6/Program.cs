ab20000ec0c40e199b9d2fa4eb4b0f5503a39216
Dinçer Tekin <dincertekin@protonmail.com>
2023-12-22 20:10:47 +0200

    Create Program.cs

---
+++ ders-6/Program.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace altinciBolum
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        int a = 5;
+        int b = 10;
+        if (a > b)
+        {
+          throw new Exception ("a, b'den buyuk olamaz!");
+        } else
+        {
+          int toplam = a + b;
+          Console.WriteLine(toplam);
+        } catch (Exception ex)
+        {
+          Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+          Console.WriteLine("Tum kodlar yurutuldu, her sey tamam. Finally calisti. ")|
+        }
+      }
+    }
+  }
+}
