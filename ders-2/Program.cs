1868bedfb5cdb92ae7febed31b2200313647bc73
Dinçer Tekin <dincertekin@protonmail.com>
2023-12-22 19:38:42 +0200

    Create Program.cs

---
+++ ders-2/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ilkprojemiz
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // Veri Türleri:
+      // int, bool, string, float, double, uint, char
+      // verituru degiskenadi = esiti;
+      int sayi = 3;
+      bool girisYaptiMi = false;
+      string isim = "Dinçer";
+      double ondalik = 1.47;
+      float virgullu = 3.1489f;
+      uint desteklenensayi = 49;
+      char karakter = 'A';
+      Console.WriteLine(sayi);
+      Console.WriteLine(girisYaptiMi);
+      Console.WriteLine(isim);
+      Console.WriteLine(ondalik);
+      Console.WriteLine(virgullu);
+      Console.WriteLine(desteklenensayi);
+      Console.WriteLine(karakter);
+    }
+  }
+}
