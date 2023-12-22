901e866f24629974503ffc9cbf98e90a8a4eec1f
Dinçer Tekin <dincertekin@protonmail.com>
2023-12-22 19:35:58 +0200

    Create Program.cs

---
+++ ders-1/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading. Tasks;
+
+namespace ilkprojemiz
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Merhaba Dünya!");
+    }
+  }
+}
