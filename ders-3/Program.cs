59f420059ec6c104be624ff385f25cac65e62e92
Dinçer Tekin <dincertekin@protonmail.com>
2023-12-22 19:52:50 +0200

    Create Program.cs

---
+++ ders-3/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ilkprojemiz
+{
+  internal class Program
+  {
+    static void Main(String[] args)
+    {
+      // Operatörler: +, -, /, -, ^, %
+      int x = 5;
+      int y = 10;
+      if (x + y > 12)
+      {
+        Console.WriteLine("Verilen iki sayı 12'den büyük!");
+      } else {
+        Console.WriteLine("Pek olmadı gibi.");
+      }
+      
+      // if: Eğer
+      // else: Değilse
+      Console.WriteLine("İsminiz nedir?");
+      string isim = Console.ReadLine();
+      if (isim == "Dinçer")
+      {
+        Console.WriteLine("Tanıdım seni " + isim);
+      } else {
+        Console.WriteLine("Aaa merhaba " + isim);
+      }
+      Console.ReadKey();
+    }
+  }
+}
